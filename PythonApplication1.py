abec959f10ce20395f68c060cd98bba8fcab352a
sun0LO <sun.azimzadeh@gmail.com>
2025-06-01 16:11:18 +0330

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,21 @@
+﻿import tkinter as tk
+
+def add_task():
+    task = entry.get()
+    if task:
+        listbox.insert(tk.END, task)
+        entry.delete(0, tk.END)
+
+root = tk.Tk()
+root.title("To-Do List")
+
+entry = tk.Entry(root, width=30)
+entry.pack(pady=10)
+
+add_button = tk.Button(root, text="Add Task", command=add_task)
+add_button.pack()
+
+listbox = tk.Listbox(root, width=40, height=10)
+listbox.pack(pady=10)
+
+root.mainloop()
